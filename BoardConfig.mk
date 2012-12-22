62ab462fff7c24bc03cc11d811d2184fa67fd7fb
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-12-21 20:57:17 -0600

    mako: tune for cortex-a9
    Change-Id: Iad9d264d9d6880f4a981c425f8fdcd6066ee8384

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 #
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+# Tune for cortex-a9
+TARGET_EXTRA_CFLAGS := -mtune=cortex-a9 -mcpu=cortex-a9
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
